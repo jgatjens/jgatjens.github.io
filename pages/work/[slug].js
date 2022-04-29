050eefa3ba2d272f4eb63f0238e468e1d6d824e4
Jay <jgatjens@gmail.com>
2022-04-29 14:08:53 -0600

    update work detail section

--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -3,7 +3,13 @@ import Meta from "../components/Meta";
 import Layout from "../components/Layout";
 import { WorkDetail } from "@/components/WorkDetail/WorkDetail";
 
-export default function WorkDetailPage({ data, nav, locale, buttonLabel }) {
+export default function WorkDetailPage({
+  data,
+  nav,
+  locale,
+  buttonLabel,
+  techTitle,
+}) {
   if (!data) return null;
 
   const seo = {
@@ -19,7 +25,7 @@ export default function WorkDetailPage({ data, nav, locale, buttonLabel }) {
     <Layout nav={nav} locale={locale}>
       <Meta data={seo} />
       <div className="flex items-center justify-center h-full flex-col">
-        <WorkDetail {...data} buttonLabel={buttonLabel} />
+        <WorkDetail {...data} techTitle={techTitle} buttonLabel={buttonLabel} />
       </div>
     </Layout>
   );
@@ -29,26 +35,12 @@ export async function getStaticProps({ params, locale }) {
   const res = await getData("projects", locale);
   const nav = await getData("navigation", locale);
   const buttonLabel = res.see_work;
+  const techTitle = res.tech_title;
 
   const data = res.items.filter((item) => item.slug == params.slug);
 
-  // const data = res.items.filter((item, index) => {
-  //   if (item.slug === params.slug) {
-  //     let next;
-  //     if (index < res.items) {
-  //       next = res.items[index + 1];
-  //     } else {
-  //       next = res.items[0].slug;
-  //     }
-
-  //     item.nextSlug = next;
-
-  //     return item;
-  //   }
-  // });
-
   return {
-    props: { data: data[0], nav, locale, buttonLabel }, // will be passed to the page component as props
+    props: { data: data[0], nav, locale, buttonLabel, techTitle }, // will be passed to the page component as props
   };
 }
 
--- src/WorkDetail/WorkDetail.jsx
+++ src/WorkDetail/WorkDetail.jsx
@@ -1,31 +1,42 @@
-// import Image from "next/image";
 import React from "react";
 import { Button } from "../Button/Button";
-/**
- * Primary UI component for user interaction
- */
+import Image from "next/image";
+
+const List = ({ items }) => (
+  <ul className="list-disc list-inside mb-8 pl-4">
+    {items.map((str, i) => {
+      if (str.length === 0) {
+        return;
+      }
+      return (
+        <li className="text-bodysmall" key={i}>
+          {str}
+        </li>
+      );
+    })}
+  </ul>
+);
+
 export const WorkDetail = ({
   title,
   date,
   image,
   description,
-  skills,
+  tech,
   buttonLabel,
+  techTitle,
 }) => (
-  <div className="max-w-xl m-auto pt-14" role="alert">
-    <h1 className="text-h1 text-bluedark  mb-14">{title}</h1>
+  <div className="max-w-xl m-auto my-16" role="alert">
+    <h1 className="text-h1 text-bluedark mb-14">{title}</h1>
     <h3 className="text-h3 font-bold mb-6"> {date}</h3>
     <p className="text-bodysmall mb-12">{description}</p>
-    <p className="text-caption mb-6">
-      <b>{skills}</b>
-    </p>
 
-    <img
-      src={image.url}
-      width="520"
-      height="340"
-      className="mb-14 rounded-md"
-    />
+    <h2 className="text-h4 text-bluedark mb-5">{techTitle}</h2>
+    <List items={tech.split("-")}></List>
+
+    <div className="mb-14 font-bold rounded-md block">
+      <Image src={image.url} width="520" height="340" />
+    </div>
 
     <Button label={buttonLabel || "SEE PROJECT"} />
   </div>
