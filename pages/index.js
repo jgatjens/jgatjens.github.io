4e5247a0ca9bd7c0ca2f6ef07ab0d62d12817603
Jay <jgatjens@gmail.com>
2022-04-30 13:06:37 -0600

    add particles animation home

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
+import Script from "next/script";
 import { Profile, Layout, Meta } from "@/components";
 import { getData } from "@/db/index";
 
+import { startAnimation } from "../public/js/animation.js";
+
 export default function Home({ data, nav, locale }) {
+  const onLoad = () => {
+    startAnimation();
+  };
+
   return (
     <Layout nav={nav} locale={locale}>
       <Meta data={data.Seo} />
 
+      <div id="css--particles"></div>
       <div className="flex items-center justify-center h-full border-b-bluedark border-b-4">
         <Profile
           title={data.profile.title}
@@ -15,6 +23,11 @@ export default function Home({ data, nav, locale }) {
           button={{ label: data.hire_me.text, link: data.hire_me.url }}
         />
       </div>
+
+      <Script
+        src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"
+        onLoad={onLoad}
+      />
     </Layout>
   );
 }
@@ -22,7 +35,7 @@ export default function Home({ data, nav, locale }) {
 export async function getStaticProps({ locale }) {
   const data = await getData("homepage", locale);
   const nav = await getData("navigation", locale);
-  // console.log(data);
+  console.log(data);
   return {
     props: { data, nav, locale }, // will be passed to the page component as props
   };
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -16,14 +16,14 @@ export function Layout({ nav, locale, color, children }) {
       <main className="w-full h-full">{children}</main>
 
       {/* bars */}
-      <div className="absolute hidden top-1/2 -left-2 -mt-3 md:block">
+      <div className="fixed hidden top-1/2 -left-2 -mt-3 md:block">
         <div className="h-[4px] block bg-bluedark rounded-md w-16 mb-1"></div>
         <div className="h-[4px] block bg-bluedark rounded-md w-24 mb-1"></div>
         <div className="h-[4px] block bg-bluedark rounded-md w-10"></div>
       </div>
 
       {/* background image */}
-      <div className="absolute bottom-0 left-0 -z-10">
+      <div className="fixed bottom-0 left-0 -z-10">
         <Image
           src="/assets/img/bg-website.jpg"
           alt="background home image"
